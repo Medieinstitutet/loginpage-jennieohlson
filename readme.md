0cadbadd7adfba7408467880f8d97a4dd62592ba
Jennie Olsson <ohlsonjosephine@gmail.com>
2021-12-05 15:09:36 +0100

    Snart färdig hehe

---
+++ readme.md
@@ -0,0 +1,14 @@
+Hej!
+
+Här kommer fler inlogg som ni kan testa på min sida! Jag har varit väldigt kreativ med lösenorden ser jag nu, borde få guldstjärna för det hehe.
+
+Användarnamn: janne
+Lösenord: test
+
+Användarnamn: jennie
+Lösenord: test
+
+Användarnamn: ture
+Lösenord: test
+
+Allt gott!! //Jennie 
---
+++ script.js
@@ -0,0 +1,82 @@
+
+
+// Här sparas alla användarnamn och lösenord som funkar att logga in med på sidan
+const allUsers = [
+    {
+        logInName: "jennie",
+        logInPassword: "test",
+    },
+    {
+        logInName: "ture",
+        logInPassword: "test",
+    },
+    {
+        logInName: "janne",
+        logInPassword: "test",
+    }
+];
+
+const storageInput = document.querySelector(".storage");
+const storedInput = localStorage.getItem("textInput");
+const button = document.querySelector(".button");
+const logOutButton = document.querySelector(".buttonLogOut");
+const text = document.querySelector(".text");
+
+// När man klickar på logga ut knappen, kallar på en funktion
+logOutButton.addEventListener("click", logOut)
+
+// När man klickar på loggar in knappen, kallar på en funktion
+button.addEventListener("click", logIn)
+
+function logIn() {
+    const userName = document.getElementById("userName").value;
+    const password = document.getElementById("password").value;
+
+    for(const user of allUsers ){
+        if(userName == user.logInName && password == user.logInPassword){
+            localStorage.setItem("user", userName);
+            setBodyDataLoggedIn();
+            return;
+        }
+    }
+    document.getElementById("error").style.display = "block";
+    document.getElementById("notLoggedIn").style.display = "none";
+}
+
+function setBodyDataLoggedIn() {
+    document.getElementById("logInForm").style.display = "none";
+    document.getElementById("logOutForm").style.display = "block";
+    document.getElementById("notLoggedIn").style.display = "none";
+    document.getElementById("loggedIn").style.display = "block";
+    document.getElementById("error").style.display = "none";
+    document.getElementById("whoIsLoggedIn").innerText = localStorage.getItem("user");
+}
+
+function setBodyDataLogOut () {
+    localStorage.clear();
+    document.getElementById("logInForm").style.display = "block";
+    document.getElementById("logOutForm").style.display = "none";
+    document.getElementById("notLoggedIn").style.display = "block";
+    document.getElementById("loggedIn").style.display = "none";
+    document.getElementById("error").style.display = "none";
+}
+
+// Kollar om någon är inloggad
+function checkLogIn() {
+    if(localStorage.getItem("user")){
+        setBodyDataLoggedIn();
+    }
+}
+
+// Hur man sparar till localStorage
+const saveToLocalStorage = (userName) => {
+    localStorage.setItem("textInput", userName)
+}
+
+// Här kallar jag på en funktion i en funktion så att allt inte blir copy paste överallt
+function logOut(){
+    setBodyDataLogOut();
+}
+
+// När man loadar sidan så körs denna funktionen
+window.onload = checkLogIn();
